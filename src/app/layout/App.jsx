d4265d37056d56a315309352fcc0b128aaa39ce3
samuraikun <tazitaziawawa@gmail.com>
2018-10-25 22:44:56 +0900

    setting router & switch separate Home and Events

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
+import { Route, Switch } from 'react-router-dom';
 import EventDashboard from '../../features/events/EventDashboard/EventDashboard'
 import NavBar from '../../features/nav/NavBar/NavBar'
+import EventForm from '../../features/events/EventForm/EventForm'
+import SettingDashboard from '../../features/user/Settings/SettingsDashboard'
+import UserDetailPage from '../../features/user/UserDetail/UserDetailPage'
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard'
+import EventDetailPage from '../../features/events/EventDetail/EventDetailPage';
+import HomePage from '../../features/home/HomePage'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path='/events' component={EventDashboard} />
+                  <Route path='/events/:id' component={EventDetailPage} />
+                  <Route path='/people' component={PeopleDashboard} />
+                  <Route path='/profile/:id' component={UserDetailPage} />
+                  <Route path='/settings' component={SettingDashboard} />
+                  <Route path='/createEvent' component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </React.Fragment>
     );
   }
