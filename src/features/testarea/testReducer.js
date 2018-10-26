92550b545fa4ded8c98832e0f78258b359720f94
samuraikun <tazitaziawawa@gmail.com>
2018-10-26 16:41:49 +0900

    Reducer内の処理をSwitch文を使わない処理にする

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,18 +1,30 @@
-import { INCREMENT_CONUNTER, DECREMENT_CONUNTER } from "./testConstants";
+import { createReducer } from '../../app/common/util/reducerUtil'
+import { INCREMENT_CONUNTER, DECREMENT_CONUNTER } from './testConstants';
 
 const initialState = {
   data: 42
 }
 
-const testReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT_CONUNTER:
-      return {...state, data: state.data + 1}
-    case DECREMENT_CONUNTER:
-      return {...state, data: state.data - 1}
-    default:
-      return state
-  }
+export const incrementCounter = (state, payload) => {
+  return {...state, data: state.data + 1}
 }
 
-export default testReducer
+export const decrementCounter = (state, payload) => {
+  return {...state, data: state.data - 1}
+}
+
+// const testReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT_CONUNTER:
+//       return {...state, data: state.data + 1}
+//     case DECREMENT_CONUNTER:
+//       return {...state, data: state.data - 1}
+//     default:
+//       return state
+//   }
+// }
+
+export default createReducer(initialState, {
+  [INCREMENT_CONUNTER]: incrementCounter,
+  [DECREMENT_CONUNTER]: decrementCounter
+})
