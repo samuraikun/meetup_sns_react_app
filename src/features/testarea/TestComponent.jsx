6e2d5280c40ae187a974d33a6b0e15c75b7a67e7
samuraikun <tazitaziawawa@gmail.com>
2018-11-03 00:05:49 +0900

    ログインフォームと新規登録フォームをモーダル表示する どのフォームをモーダル表示するかは、発行されたActionをModalManager Componentが、 監視して、どのフォームをモーダル表示するかを決める

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import GoogleMapReact from 'google-map-react'
-import { Button, Icon } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import Script from 'react-load-script'
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import { incrementCounter, decrementCounter } from './testActions'
+import { openModal } from '../modals/modalActions'
 
 const mapStateToProps = state => ({
   data: state.test.data
@@ -12,10 +12,9 @@ const mapStateToProps = state => ({
 
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 }
-
-const Marker = () => <Icon name='marker' size='big' color='red' />
 
 class TestComponent extends Component {
   static defaultProps = {
@@ -51,38 +50,25 @@ class TestComponent extends Component {
       value: this.state.address,
       onChange: this.onChange,
     }
-    const { incrementCounter, decrementCounter, data } = this.props
+    const { incrementCounter, decrementCounter, data, openModal } = this.props
 
     return (
       <div>
-        {/*<Script
+        <Script
           url='https://maps.googleapis.com/maps/api/js?key=AIzaSyA7OvTM0xL_349g0OHvz4EvRJ-vmlI8q8A&libraries=places'
           onLoad={this.handleScriptLoad}
-        />*/}
+        />
         <h1>Test area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color='green' content='Increment' />
         <Button onClick={decrementCounter} color='red' content='Decrement' />
+        <Button onClick={() => openModal('TestModal', {data: 43})} color='red' content='Open Modal' />
         <br />
         <br />
         <form onSubmit={this.handleFormSubmit}>
         { this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} /> }
         <button type="submit">Submit</button>
-      </form>
-
-      <div style={{ height: '300px', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyA7OvTM0xL_349g0OHvz4EvRJ-vmlI8q8A' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <Marker
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
-      </div>
+        </form>
       </div>
     )
   }
