11124738ef624a39eb5f963a301cbb506ee2ee38
samuraikun <tazitaziawawa@gmail.com>
2018-11-02 20:22:35 +0900

    cosmetic

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import GoogleMapReact from 'google-map-react'
+import { Button, Icon } from 'semantic-ui-react'
 import Script from 'react-load-script'
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import { incrementCounter, decrementCounter } from './testActions'
@@ -14,7 +15,17 @@ const actions = {
   decrementCounter
 }
 
+const Marker = () => <Icon name='marker' size='big' color='red' />
+
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  }
+
   state = {
     address: '',
     scriptLoaded: false
@@ -44,10 +55,10 @@ class TestComponent extends Component {
 
     return (
       <div>
-        <Script
+        {/*<Script
           url='https://maps.googleapis.com/maps/api/js?key=AIzaSyA7OvTM0xL_349g0OHvz4EvRJ-vmlI8q8A&libraries=places'
           onLoad={this.handleScriptLoad}
-        />
+        />*/}
         <h1>Test area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color='green' content='Increment' />
@@ -58,6 +69,20 @@ class TestComponent extends Component {
         { this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} /> }
         <button type="submit">Submit</button>
       </form>
+
+      <div style={{ height: '300px', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyA7OvTM0xL_349g0OHvz4EvRJ-vmlI8q8A' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <Marker
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
       </div>
     )
   }
