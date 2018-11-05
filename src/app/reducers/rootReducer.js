1f64ddc6dd3ad9068686518f8e83d7cfeb492065
samuraikun <tazitaziawawa@gmail.com>
2018-11-05 12:51:49 +0900

    add new redux flow about authentication.

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { reducer as FormReducer } from 'redux-form'
 import testReducer from '../../features/testarea/testReducer'
 import eventReducer from '../../features/events/eventReducer'
 import modalsReducer from '../../features/modals/modalReducer'
+import authReducer from '../../features/auth/authReducer'
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
-  modals: modalsReducer
+  modals: modalsReducer,
+  auth: authReducer
 })
 
 export default rootReducer
