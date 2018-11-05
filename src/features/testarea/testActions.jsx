10f62e8b3ac7cf12364f6729c8d40422bb2f70ee
samuraikun <tazitaziawawa@gmail.com>
2018-11-05 14:02:58 +0900

    add Redux Thunk & sample redux thunk code in testarea

--- src/features/testarea/testActions.jsx
+++ src/features/testarea/testActions.jsx
@@ -1,4 +1,4 @@
-import { INCREMENT_CONUNTER, DECREMENT_CONUNTER } from './testConstants'
+import { INCREMENT_CONUNTER, DECREMENT_CONUNTER, COUNTER_ACTION_STARTED, COUNTER_ACTION_FINISHED } from './testConstants'
 
 export const incrementCounter = () => {
   return {
@@ -9,5 +9,39 @@ export const incrementCounter = () => {
 export const decrementCounter = () => {
   return {
     type: DECREMENT_CONUNTER
+  }
+}
+
+export const startCounterAction = () => {
+  return {
+    type: COUNTER_ACTION_STARTED
+  }
+}
+
+export const finishCounterAction = () => {
+  return {
+    type: COUNTER_ACTION_FINISHED
+  }
+}
+
+export const delay = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export const incrementAsync = () => {
+  return async dispatch => {
+    dispatch(startCounterAction())
+    await delay(1000)
+    dispatch({type: INCREMENT_CONUNTER})
+    dispatch(finishCounterAction())
+  }
+}
+
+export const decrementAsync = () => {
+  return async dispatch => {
+    dispatch(startCounterAction())
+    await delay(1000)
+    dispatch({type: DECREMENT_CONUNTER})
+    dispatch(finishCounterAction())
   }
 }
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,5 +1,5 @@
 import { createReducer } from '../../app/common/util/reducerUtil'
-import { INCREMENT_CONUNTER, DECREMENT_CONUNTER } from './testConstants';
+import { INCREMENT_CONUNTER, DECREMENT_CONUNTER, COUNTER_ACTION_STARTED, COUNTER_ACTION_FINISHED } from './testConstants';
 
 const initialState = {
   data: 42
@@ -11,6 +11,14 @@ export const incrementCounter = (state, payload) => {
 
 export const decrementCounter = (state, payload) => {
   return {...state, data: state.data - 1}
+}
+
+export const counterActionStarted = (state, payload) => {
+  return {...state, loading: true}
+}
+
+export const counterActionFinished = (state, payload) => {
+  return {...state, loading: false}
 }
 
 // const testReducer = (state = initialState, action) => {
@@ -26,5 +34,7 @@ export const decrementCounter = (state, payload) => {
 
 export default createReducer(initialState, {
   [INCREMENT_CONUNTER]: incrementCounter,
-  [DECREMENT_CONUNTER]: decrementCounter
+  [DECREMENT_CONUNTER]: decrementCounter,
+  [COUNTER_ACTION_STARTED]: counterActionStarted,
+  [COUNTER_ACTION_FINISHED]: counterActionFinished
 })
