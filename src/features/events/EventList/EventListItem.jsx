e5f632e70c81ef7c9d9e92dcc591e507e443366d
samuraikun <tazitaziawawa@gmail.com>
2018-11-08 12:22:49 +0900

    fix error about firestore timestamp setting.

--- src/features/events/EventList/EventListItem.jsx
+++ src/features/events/EventList/EventListItem.jsx
@@ -26,8 +26,8 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon name="clock" /> {format(event.date, 'YYYY/MM/DD', { locale: japan })} {' '}
-            {format(event.date, 'HH:mm')} |
+            <Icon name="clock" /> {format(event.date.toDate(), 'YYYY/MM/DD', { locale: japan })} {' '}
+            {format(event.date.toDate(), 'HH:mm')} |
             <Icon name="marker" /> {event.venue}
           </span>
         </Segment>
