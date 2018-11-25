0ebd13d8a2c287ccb025d5f40fe479889d663aa7
samuraikun <tazitaziawawa@gmail.com>
2018-11-25 16:32:09 +0900

    connect PhotoPage Component to Firestore

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import { Image, Segment, Header, Divider, Grid, Button, Card, Icon } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
 import Cropper from 'react-cropper';
@@ -7,9 +9,25 @@ import 'cropperjs/dist/cropper.css';
 import { toastr } from 'react-redux-toastr';
 import { uploadProfileImage } from '../userActions';
 
+const query = ({ auth }) => {
+  return [
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{collection: 'photos'}],
+      storeAs: 'photos'
+    }
+  ];
+}
+
 const actions = {
   uploadProfileImage
 }
+
+const mapStateToProps = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
+});
 
 class PhotosPage extends Component {
   state = {
@@ -17,7 +35,7 @@ class PhotosPage extends Component {
     fileName: '',
     cropResult: null,
     image: {}
-  } 
+  }
 
   uploadImage = async () => {
     try {
@@ -130,4 +148,7 @@ class PhotosPage extends Component {
   }
 }
 
-export default connect(null, actions)(PhotosPage);
+export default compose(
+  connect(mapStateToProps, actions),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
