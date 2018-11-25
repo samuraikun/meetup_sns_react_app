7c6a77d1a435c9e22533722f3deee8d860f3c8bc
samuraikun <tazitaziawawa@gmail.com>
2018-11-25 16:59:20 +0900

    画像ファイル名の重複を防ぐ

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import cuid from 'cuid';
 import { toastr } from 'react-redux-toastr';
 
 export const updateProfile = user => async (dispatch, getState, { getFirebase }) => {
@@ -18,12 +19,13 @@ export const updateProfile = user => async (dispatch, getState, { getFirebase })
 }
 
 export const uploadProfileImage = (file, fileName) => async (dispatch, getState, { getFirebase, getFirestore }) => {
+  const imageName = cuid();
   const firebase = getFirebase();
   const firestore = getFirestore();
   const user = firebase.auth().currentUser;
   const path = `${user.uid}/user_images`;
   const options = {
-    name: fileName
+    name: imageName
   };
 
   try {
@@ -49,7 +51,7 @@ export const uploadProfileImage = (file, fileName) => async (dispatch, getState,
       doc: user.uid,
       subcollections: [{collection: 'photos'}]
     }, {
-      name: fileName,
+      name: imageName,
       url: downloadURL
     });
   } catch (error) {
