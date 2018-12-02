b3518d0ce318b4d9425f4128f3188fc0a874b97a
samuraikun <tazitaziawawa@gmail.com>
2018-12-02 15:39:04 +0900

    show event activities

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -7,9 +7,18 @@ import { getEventsForDashboard } from '../eventActions'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import EventActivity from '../EventActivity/EventActivity'
 
+const query = [
+  {
+    collection: 'activity',
+    orderBy: ['timestamp', 'desc'],
+    limit: 5
+  }
+];
+
 const mapStateToProps = state => ({
   events: state.events,
-  loading: state.async.loading
+  loading: state.async.loading,
+  activities: state.firestore.ordered.activity 
 })
 
 const actions = {
@@ -55,7 +64,7 @@ class EventDashboard extends Component {
   }
 
   render() {
-    const { loading } = this.props;
+    const { loading, activities } = this.props;
     const { moreEvents, loadedEvents } = this.state;
 
     if (this.state.loadingInitial) return <LoadingComponent inverted={true} />
@@ -71,7 +80,7 @@ class EventDashboard extends Component {
           />
         </Grid.Column>
         <Grid.Column width={6}>
-          <EventActivity />
+          <EventActivity activities={activities} />
         </Grid.Column>
         <Grid.Column width={10}>
           <Loader active={loading} />
@@ -82,5 +91,5 @@ class EventDashboard extends Component {
 }
 
 export default connect(mapStateToProps, actions)(
-  firestoreConnect([{ collection: 'events' }])(EventDashboard)
+  firestoreConnect(query)(EventDashboard)
 );
