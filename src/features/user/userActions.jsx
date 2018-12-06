c895d345b9a2266557333b2cd6589273e1f82311
samuraikun <tazitaziawawa@gmail.com>
2018-12-06 17:31:16 +0900

    イベント参加のアクションは、トランザクション処理にする - Userドキュメントを元にattendee コレクションを作成するので、User 情報が更新中のタイミングなどを考慮している

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -136,9 +136,11 @@ export const setMainPhoto = photo => async (dispatch, getState) => {
   }
 }
 
-export const goingToEvent = event => async (dispatch, getState, { getFirestore }) => {
-  const firestore = getFirestore();
-  const user = firestore.auth().currentUser;
+export const goingToEvent = event => async (dispatch, getState) => {
+  dispatch(asyncActionStart());
+
+  const firestore = firebase.firestore();
+  const user = firebase.auth().currentUser;
   const photoURL = getState().firebase.profile.photoURL || '/assets/user.png';
   const attendee = {
     going: true,
@@ -149,20 +151,27 @@ export const goingToEvent = event => async (dispatch, getState, { getFirestore }
   }
 
   try {
-    await firestore.update(`events/${event.id}`, {
-      [`attendees.${user.uid}`]: attendee
+    let eventDocRef = firestore.collection('events').doc(event.id);
+    let eventAttendeeDocRef = firestore.collection('event_attendee').doc(`${event.id}_${user.uid}`);
+
+    await firestore.runTransaction(async transaction => {
+      await transaction.get(eventDocRef);
+      await transaction.update(eventDocRef, {
+        [`attendees.${user.uid}`]: attendee
+      });
+      await transaction.set(eventAttendeeDocRef, {
+        eventId: event.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: false
+      });
     });
 
-    await firestore.set(`event_attendee/${event.id}_${user.uid}`, {
-      eventId: event.id,
-      userUid: user.uid,
-      eventDate: event.date,
-      host: false
-    });
-
+    dispatch(asyncActionFinish());
     toastr.success('Success', 'You have signed up to the event');
   } catch (error) {
     console.log(error);
+    dispatch(asyncActionError());
     toastr.error('Oops', 'Problem signing up to event')
   }
 }
